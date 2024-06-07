5079ebb6afc9fd8573291c4ae291eccd6cfac368
Vivek Pathak <vivekpathak8198@gmail.com>
2024-06-07 13:50:25 +0530

    adding main component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Main from "./components/main/Main";
 
 function App() {
   return (
@@ -7,6 +7,10 @@ function App() {
     <div className="bg-gray-300 h-full">
       <div>
         <Navbar />
+      </div>
+      {/*This is main body of crypto currency dashboard*/}
+      <div>
+        <Main />
       </div>
     </div>
   );
