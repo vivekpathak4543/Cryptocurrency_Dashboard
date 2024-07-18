a41db5d2fadcd05c9b7a751d2328cc489d7b0974
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-18 21:18:09 +0530

    fix error in code

--- src/components/heading/SearchOption.js
+++ src/components/heading/SearchOption.js
@@ -1,21 +1,85 @@
+// import React, { useState } from "react";
+// import { useSelector } from "react-redux";
+
+// //this component contains SearchOption we show coin details during searching
+// const SearchOption = () => {
+//   const [input, setInput] = useState();
+//   const [result, setResult] = useState([]);
+//   const coin = useSelector((state) => state.coinReducer.currencies);
+
+//   function coinHandler(value) {
+//     setInput(value);
+//     //Filter the coin during search
+//     const coinResult = coin.filter((item) => {
+//       return (
+//         value && item && item.name && item.name.toLowerCase().includes(value)
+//       );
+//     });
+//     setResult(coinResult);
+//   }
+
+//   return (
+//     <div className="w-full">
+//       <div className="flex gap-1">
+//         <div className="relative w-full">
+//           <input
+//             className="w-full h-9 rounded-md text-sm pl-5 focus:outline-none"
+//             onChange={(e) => coinHandler(e.target.value)}
+//             value={input}
+//             type="search"
+//             placeholder="Search by coin name"
+//           />
+//         </div>
+//         <button className="bg-blue-600 h-9 w-[3.2rem] text-white font-semibold text-sm rounded-md">
+//           Search
+//         </button>
+//       </div>
+//       <div className="absolute w-[59%]">
+//         {result.map((item, id) => {
+//           return (
+//             <div
+//               className="flex bg-white pt-1 rounded-md w-full item-center"
+//               key={id}
+//             >
+//               <img
+//                 className="w-[50px] h-[50px] p-1 ml-2"
+//                 src={item.image}
+//                 alt={item.name}
+//               />
+//               <p className="p-2">{item.name}</p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SearchOption;
+
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-//this component contains SearchOption we show coin details during searching
+// This component contains SearchOption to show coin details during searching
 const SearchOption = () => {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const [result, setResult] = useState([]);
   const coin = useSelector((state) => state.coinReducer.currencies);
 
   function coinHandler(value) {
     setInput(value);
-    //Filter the coin during search
+    // Filter the coins during search
     const coinResult = coin.filter((item) => {
       return (
-        value && item && item.name && item.name.toLowerCase().includes(value)
+        value && item && item.name && item.name.toLowerCase().includes(value.toLowerCase())
       );
     });
     setResult(coinResult);
+  }
+
+  function selectCoin(name) {
+    setInput(name);
+    setResult([]); // Clear the search results
   }
 
   return (
@@ -34,12 +98,13 @@ const SearchOption = () => {
           Search
         </button>
       </div>
-      <div className="absolute w-[59%]">
-        {result.map((item, id) => {
-          return (
+      {result.length > 0 && (
+        <div className="absolute w-[59%] bg-white rounded-md shadow-lg mt-2">
+          {result.map((item, id) => (
             <div
-              className="flex bg-white pt-1 rounded-md w-full item-center"
+              className="flex items-center p-2 cursor-pointer hover:bg-gray-200"
               key={id}
+              onClick={() => selectCoin(item.name)}
             >
               <img
                 className="w-[50px] h-[50px] p-1 ml-2"
@@ -48,9 +113,9 @@ const SearchOption = () => {
               />
               <p className="p-2">{item.name}</p>
             </div>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
